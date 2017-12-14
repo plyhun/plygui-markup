b7ace99d0bef2df4f285d0a77ff3a642208d029d
Serhii Plyhun <snuk188@gmail.com>
2017-12-14 17:54:01 +0100

    Markup parse works

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,7 @@ pub use plygui_api::markup;
 
 use std::collections::HashMap;
 
-const ID: &str = "id";
-const TYPE: &str = "type";
-
-pub fn parse_markup<F>(json: &str, constructor: F) -> (Box<traits::UiControl>, markup::MarkupIds) where F: Fn(&str, &markup::Markup) -> Box<traits::UiControl> {
+pub fn parse_markup<F>(json: &str, constructor: F) -> (Box<traits::UiControl>, markup::MarkupIds) where F: Fn(&markup::Markup) -> Box<traits::UiControl> {
 	let mut ids: markup::MarkupIds = HashMap::new();
 	let markup: markup::Markup = serde_json::from_str(json).unwrap();
 	
@@ -20,20 +17,6 @@ pub fn parse_markup<F>(json: &str, constructor: F) -> (Box<traits::UiControl>, m
 	(member, ids)
 }
 
-fn parse_node<F>(node: &markup::Markup, ids: &mut markup::MarkupIds, constructor: F) -> Box<traits::UiControl> where F: Fn(&str, &markup::Markup) -> Box<traits::UiControl> {
-	if let Some(a_type) = node.get(TYPE) {
-		let member = match *a_type {
-			markup::MarkupNode::Attribute(ref a_type) => constructor(a_type, &node),
-			_ => panic!("\"type\" can only be a plain String attribute, {:?} found instead", a_type),
-		};
-		if let Some(id) = node.get(ID) {
-			match *id {
-				markup::MarkupNode::Attribute(ref id) => ids.insert(id.clone(), member.id()),
-				_ => panic!("\"id\" can only be a plain String attribute, {:?} found instead", id),
-			};
-		}
-		member
-	} else {
-		panic!("Markup does not have a \"type\": {:?}", node)
-	}
+fn parse_node<F>(node: &markup::Markup, ids: &mut markup::MarkupIds, constructor: F) -> Box<traits::UiControl> where F: Fn(&markup::Markup) -> Box<traits::UiControl> {
+	constructor(node)
 }
