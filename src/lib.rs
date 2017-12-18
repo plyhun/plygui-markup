1a47a4317d1d9c7efea260e512be5f5c1bdc10d5
Serhii Plyhun <snuk188@gmail.com>
2017-12-18 18:11:31 +0100

    Markup ecosystem.

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,65 @@ use plygui_api::*;
 
 pub use plygui_api::markup;
 
-use std::collections::HashMap;
+use std::sync::{Once, ONCE_INIT};
 
-pub fn parse_markup<F>(json: &str, constructor: F) -> (Box<traits::UiControl>, markup::MarkupIds) where F: Fn(&markup::Markup) -> Box<traits::UiControl> {
-	let mut ids: markup::MarkupIds = HashMap::new();
-	let markup: markup::Markup = serde_json::from_str(json).unwrap();
-	
-	let member = parse_node(&markup, &mut ids, constructor);
-	(member, ids)
+pub enum MarkupError {
+	MemberNotFound,
+	MemberAlreadyRegistered,
 }
 
-fn parse_node<F>(node: &markup::Markup, ids: &mut markup::MarkupIds, constructor: F) -> Box<traits::UiControl> where F: Fn(&markup::Markup) -> Box<traits::UiControl> {
-	constructor(node)
+static mut IDS: Option<markup::MarkupIds> = None;
+static mut REGISTRY: Option<markup::MarkupRegistry> = None;
+static INIT: Once = ONCE_INIT;
+
+fn init_registry() {
+	unsafe { 
+		REGISTRY = Some(Default::default()); 
+		IDS = Some(Default::default());
+	}
+}
+
+pub fn registry<'a>() -> &'a markup::MarkupRegistry {
+	INIT.call_once(init_registry);
+	unsafe { REGISTRY.as_ref().unwrap() }
+}
+pub fn registry_mut<'a>() -> &'a mut markup::MarkupRegistry {
+	INIT.call_once(init_registry);
+	unsafe { REGISTRY.as_mut().unwrap() }
+}
+
+pub fn register_member(member_id: &str, member_spawner: fn() -> Box<plygui_api::traits::UiControl>) -> Result<(), MarkupError> {
+	INIT.call_once(init_registry);
+	unsafe { 
+		let registry = REGISTRY.as_mut().unwrap();
+		if registry.get(member_id).is_none() {
+			registry.insert(member_id.into(), member_spawner); 
+			Ok(())
+		} else {
+			Err(MarkupError::MemberAlreadyRegistered)
+		}
+	}
+}
+
+pub fn unregister_member(member_id: &str) -> Result<(), MarkupError> {
+	INIT.call_once(init_registry);
+	unsafe { 
+		let registry = REGISTRY.as_mut().unwrap();
+		if registry.remove(member_id).is_none() {
+			Err(MarkupError::MemberNotFound)
+		} else {
+			Ok(())
+		}
+	}
+}
+
+pub fn parse_markup(json: &str) -> Box<traits::UiControl> {
+	INIT.call_once(init_registry);
+	
+	let markup: markup::Markup = serde_json::from_str(json).unwrap();
+	
+	let registry = unsafe { REGISTRY.as_ref().unwrap() };
+	let mut control = registry.get(&markup.member_type).unwrap()();
+	control.fill_from_markup(&markup, registry, unsafe { IDS.as_mut().unwrap() } );
+	control
 }
