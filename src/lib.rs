fbf3ef4f1db297e1a6f4a08b0de9205be1ba7535
Serhii Plyhun <snuk188@gmail.com>
2018-05-28 11:16:29 +0200

    Get rid of prefixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate plygui_api;
 
-pub use plygui_api::traits::*;
+pub use plygui_api::controls::*;
 pub use plygui_api::ids::*;
 pub use plygui_api::types::*;
 pub use plygui_api::callbacks;
@@ -9,25 +9,17 @@ pub use plygui_api::utils;
 pub use plygui_api::markup;
 
 #[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
-extern crate plygui_gtk;
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
-pub use plygui_gtk::*;
+pub extern crate plygui_gtk as imp;
 
 #[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
-extern crate plygui_qt;
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
-pub use plygui_qt::*;
+pub extern crate plygui_qt as imp;
 
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
-extern crate plygui_cocoa;
-#[cfg(all(target_os = "macos", feature = "cocoa"))]
-pub use plygui_cocoa::*;
+pub extern crate plygui_cocoa as imp;
 
 #[cfg(all(target_os = "windows", feature = "win32"))]
-extern crate plygui_win32;
-#[cfg(all(target_os = "windows", feature = "win32"))]
-pub use plygui_win32::*;
+pub extern crate plygui_win32 as imp;
 
 pub fn register_markup_members(registry: &mut plygui_api::markup::MarkupRegistry) {
-    register_members(registry);
+    imp::register_members(registry);
 }
