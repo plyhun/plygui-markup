5ee9e796fba1634d9d9328890fa2807b9f1785bc
snuk182 <snuk188@gmail.com>
2018-09-14 20:17:56 +0200

    New API

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate plygui_api;
 
-pub use plygui_api::traits::*;
+pub use plygui_api::controls::*;
 pub use plygui_api::ids::*;
 pub use plygui_api::types::*;
 pub use plygui_api::callbacks;
@@ -8,25 +8,33 @@ pub use plygui_api::layout;
 pub use plygui_api::utils;
 pub use plygui_api::markup;
 
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
+#[cfg(feature = "gtk3")]
 extern crate plygui_gtk;
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "gtk3"))]
-pub use plygui_gtk::*;
+#[cfg(feature = "gtk3")]
+pub use plygui_gtk::prelude::*;
+#[cfg(feature = "gtk3")]
+pub use plygui_gtk::register_members;
 
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
+#[cfg(feature = "qt5")]
 extern crate plygui_qt;
-#[cfg(all(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"), feature = "qt5"))]
-pub use plygui_qt::*;
+#[cfg(feature = "qt5")]
+pub use plygui_qt::prelude::*;
+#[cfg(feature = "qt5")]
+pub use plygui_qt::register_members;
 
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
 extern crate plygui_cocoa;
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
-pub use plygui_cocoa::*;
+pub use plygui_cocoa::prelude::*;
+#[cfg(all(target_os = "macos", feature = "cocoa"))]
+pub use plygui_cocoa::register_members;
 
 #[cfg(all(target_os = "windows", feature = "win32"))]
 extern crate plygui_win32;
 #[cfg(all(target_os = "windows", feature = "win32"))]
-pub use plygui_win32::*;
+pub use plygui_win32::prelude::*;
+#[cfg(all(target_os = "windows", feature = "win32"))]
+pub use plygui_win32::register_members;
 
 pub fn register_markup_members(registry: &mut plygui_api::markup::MarkupRegistry) {
     register_members(registry);
