185e385f6833e4acd264a24c35063d3ce494d8f2
Serhii Plyhun <snuk188@gmail.com>
2020-01-27 22:47:38 +0100

    Spawn markup support

--- src/lib.rs
+++ src/lib.rs
@@ -11,30 +11,37 @@ pub use plygui_api::markup;
 #[cfg(feature = "gtk3")]
 extern crate plygui_gtk;
 #[cfg(feature = "gtk3")]
-pub use plygui_gtk::*;
+pub use plygui_gtk::imp;
 #[cfg(feature = "gtk3")]
 pub use plygui_gtk::register_members;
 
 #[cfg(feature = "qt5")]
 extern crate plygui_qt;
 #[cfg(feature = "qt5")]
-pub use plygui_qt::*;
+pub use plygui_qt::imp;
 #[cfg(feature = "qt5")]
 pub use plygui_qt::register_members;
 
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
 extern crate plygui_cocoa;
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
-pub use plygui_cocoa::*;
+pub use plygui_cocoa::imp;
 #[cfg(all(target_os = "macos", feature = "cocoa"))]
 pub use plygui_cocoa::register_members;
 
 #[cfg(all(target_os = "windows", feature = "win32"))]
 extern crate plygui_win32;
 #[cfg(all(target_os = "windows", feature = "win32"))]
-pub use plygui_win32::*;
+pub use plygui_win32::imp;
 #[cfg(all(target_os = "windows", feature = "win32"))]
 pub use plygui_win32::register_members;
+
+#[cfg(not(any(feature = "gtk3", feature = "cocoa", feature = "win32")))]
+extern crate plygui_testable;
+#[cfg(not(any(feature = "gtk3", feature = "cocoa", feature = "win32")))]
+pub use plygui_testable::imp;
+#[cfg(not(any(feature = "gtk3", feature = "cocoa", feature = "win32")))]
+pub use plygui_testable::register_members;
 
 pub fn register_markup_members(registry: &mut plygui_api::markup::MarkupRegistry) {
     register_members(registry);
